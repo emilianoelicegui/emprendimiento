07cc4f13f349ba71b6845b1881334d28e542e1e2
eelicegui <emielicegui@gmail.com>
2020-04-12 19:15:07 -0300

    aregago el front

---
+++ WebsiteClient/wwwroot/js/controller/Account/Login.js
@@ -0,0 +1,25 @@
+﻿/// <reference path="../../knockout-3.5.1.js" />
+/// <reference path="../../knockout.validation.js" />
+
+var Login = function (options) {
+
+    var self = this;
+
+    self.viewErrors = ko.observable(true);
+    self.email = ko.observable("").extend({ required: { message: 'Ingrese email válido' }, email: true });
+    self.password = ko.observable("").extend({ required: { message: 'Ingrese una contraseña' } });
+
+    self.errors = ko.validation.group(self);
+
+    self.clickLogin = function () {
+       
+        if (self.errors().length == 0) {
+            alert('Thank you.');
+        } else {
+            alert('Please check your submission.');
+            console.log(self.viewErrors);
+            self.errors.showAllMessages();
+        }
+
+    }
+};
