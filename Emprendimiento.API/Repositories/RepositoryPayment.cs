81d7e943fe2e79ebe26887eba101d7b8d205a62f
Emiliano Elicegui <emielicegui@gmail.com>
2021-06-11 00:16:34 -0300

    repo payment

---
+++ Emprendimiento.API/Repositories/RepositoryPayment.cs
@@ -0,0 +1,50 @@
+﻿}using Domain.Layer;
+using Microsoft.EntityFrameworkCore;
+using Shared.Layer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Emprendimiento.API.Repositories
+{
+    public interface IRepositoryPayment
+    {
+        Task<IEnumerable<Payment>> GetAllByCompany(string nombre, int idUser, int? idCompany, int start, int length);
+    }
+
+    public class RepositoryPayment : IRepositoryPayment
+    {
+        private readonly ApplicationDbContext _context;
+
+
+        public RepositoryPayment(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Payment>> GetAllByCompany(string nombre, int idUser, int? idCompany, int start, int length)
+        {
+            var query = _context.Payments
+                                .Where(x => x.Client.Company.IdUser == idUser)
+                                .Include(x => x.Client)
+                                .ThenInclude(x => x.Company)
+                                .AsQueryable();
+
+            if (!nombre.IsNullOrEmpty())
+            {
+                query = query.Where(x => x.Client.Name.Contains(nombre));
+            }
+
+            if (idCompany.HasValue)
+            {
+                query = query.Where(x => x.Client.Company.Id == idCompany);
+            }
+
+            return await query
+                    .OrderBy(x => x.Client.Name)
+                    .Skip(start)
+                    .Take(length).ToListAsync();
+        }
+    }
+}
